f3bd60c300350879e3c7c3403a214f5f68e4f76b
kkusiaku <kusiaku.k@gmail.com>
2021-01-19 09:25:42 +0100

    add simple onData function called check in unit test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,18 +3,19 @@ import pytest
 from unittest.mock import patch
 from unittest.mock import MagicMock
 import getpass
-import os 
+import os
 import shutil
 import tempfile
 
-class TestMain:
+
+class TestApp_StartUp:
   @pytest.fixture(autouse=True)
   def cleandir(self):
       old_cwd = os.getcwd()
       newpath = tempfile.mkdtemp()
       os.chdir(newpath)
       os.makedirs('./source_dir')
-      open('./source_dir/file.txt','w').close()
+      open('./source_dir/file.txt', 'w').close()
       os.makedirs('./results')
       os.makedirs('./destination')
       yield
@@ -39,6 +40,32 @@ class TestMain:
       main.rotate('./source_dir/file.txt', './results')
     assert error.exconly() == "ValueError: destination directory should be present on app start"
 
+
+class TestApp_DataProcess:
+    def cleandir(self):
+      old_cwd = os.getcwd()
+      newpath = tempfile.mkdtemp()
+      os.chdir(newpath)
+      os.makedirs('./source_dir')
+
+      with open('./source_dir/file.txt', 'w') as inputFile:
+        inputFile.write(
+            'Sepal\tlength\tSepal\twidth\tPetal\tlength\tPetal\twidth\tSpecie\n')
+        inputFile.write('5.1\t3.5\t1.4\0.2\tI.\tsetosa\n')
+        inputFile.write('4.9\t3.0\t1.4\t0.2\tI.\tsetosa\n')
+        inputFile.write('4.7\t3.2\t1.3\t0.2\tI.\tsetosa\n')
+      inputFile.close()
+
+      open('./source_dir/file.txt', 'w').close()
+
+      yield
+      os.chdir(old_cwd)
+      shutil.rmtree(newpath)
+
+    def shoul_call_onData(self):
+      onDataMock = MagicMock()
+      main.rotate('./source_dir/file.txt', './results', onData=onDataMock)
+      onDataMock.assert_called()
 
  
   
